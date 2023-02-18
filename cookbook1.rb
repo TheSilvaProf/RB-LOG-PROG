37116b9a44af9b9bb3f6593dbdba32f0d861dcd6
TheSilvaProf <fabio01@protonmail.ch>
2023-02-18 18:17:48 -0300

    Revisão aray RB

--- cookbook1.rb
+++ cookbook1.rb
@@ -1,6 +1,8 @@
 puts "******** Bem vindo ao cookbook **********"
 puts "-"
+receitas = []
 print "> Digite o nome da receita: "
 nome = gets.chomp()
 puts "-"
+receitas << nome
 puts "> Receita #{nome} cadastrada com sucesso."

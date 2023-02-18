6a5d8d8392719521d6d5ef72cf53b14a62efece7
TheSilvaProf <fabio01@protonmail.ch>
2023-02-18 19:17:11 -0300

    MEtodos RB

--- cookbook1.rb
+++ cookbook1.rb
@@ -1,36 +1,43 @@
 puts "******** Bem vindo ao cookbook **********"
 puts "-"
+def menu()
+    puts "[1] Cadastrar receita."
+    puts "[2] Ver receitas."
+    puts "[3] Sair."
+    print "> Escolha uma opção: "
+    return gets.to_i
+end
+def inserir()
+    print "> Digite o nome da receita: "
+    nome = gets.chomp()
+    print "> Digite o tipo da receita: "
+    tipo = gets.chomp()
+    puts "-"
+    puts "> Receita #{nome} cadastrada com sucesso."
+    return {nome: nome, tipo: tipo}  
+end
+def ver(receitas)
+    puts "*********** RECEITAS ***********"
+    receitas.each do |receita|
+        puts "NOME: #{receita[:nome]} -- TIPO: #{receita[:tipo]}"
+    end
+    puts "-"    
+end
+
+opcao = menu()
 receitas = []
-puts "[1] Cadastrar receita."
-puts "[2] Ver receitas."
-puts "[3] Sair."
-print "Escolha uma opção: "
-opcao = gets.to_i
+
 
 while (opcao != 3) do
     if (opcao == 1)
-        print "> Digite o nome da receita: "
-        nome = gets.chomp()
-        print "> Digite o tipo da receita: "
-        tipo = gets.chomp()
-        receitas << {nome: nome, tipo: tipo}
-        puts "-"
-        puts "> Receita #{nome} cadastrada com sucesso."
+        receitas << inserir()
     elsif (opcao == 2)
-        puts "*********** RECEITAS ***********"
-        receitas.each do |receita|
-            puts "NOME: #{receita[:nome]} -- TIPO: #{receita[:tipo]}"
-        end
-        puts "-"
+        ver(receitas)
     else
         puts "> Opção inválida!"
     end
     puts "-"
-    puts "[1] Cadastrar receita."
-    puts "[2] Ver receitas."
-    puts "[3] Sair."
-    print "Escolha uma opção: "
-    opcao = gets.to_i
+    opcao = menu()
 end
 puts "-"
 puts "*********** TCHAUUUUU! *************"

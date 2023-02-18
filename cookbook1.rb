2433982155d6a15e8fcd726fe856741ac88e0cbb
TheSilvaProf <fabio01@protonmail.ch>
2023-02-18 18:46:20 -0300

    Estrut. condicionais Rb

--- cookbook1.rb
+++ cookbook1.rb
@@ -1,18 +1,35 @@
 puts "******** Bem vindo ao cookbook **********"
 puts "-"
 receitas = []
+puts "[1] Cadastrar receita."
+puts "[2] Ver receitas."
+puts "[3] Sair."
+print "Escolha uma opção: "
+opcao = gets.to_i
 
-while (true) do
-    print "> Digite o nome da receita: "
-    nome = gets.chomp()
-    receitas << nome
-    
-    puts "-"
-    puts "> Receita #{nome} cadastrada com sucesso."
-    puts "-"
-    puts "*********** RECEITAS ***********"
-    receitas.each do |receita|
-        puts receita
+while (opcao != 3) do
+    if (opcao == 1)
+        print "> Digite o nome da receita: "
+        nome = gets.chomp()
+        receitas << nome
+        puts "-"
+        puts "> Receita #{nome} cadastrada com sucesso."
+        puts "-"
+    elsif (opcao == 2)
+        puts "*********** RECEITAS ***********"
+        receitas.each do |receita|
+            puts receita
+        end
+        puts "-"
+    else
+        puts "> Opção inválida!"
     end
     puts "-"
+    puts "[1] Cadastrar receita."
+    puts "[2] Ver receitas."
+    puts "[3] Sair."
+    print "Escolha uma opção: "
+    opcao = gets.to_i
 end
+puts "-"
+puts "*********** TCHAUUUUU! *************"
